7f273fd5a8ff0d8fdbd179833bc44cf5bd1e41c5
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-08-01 22:29:49 +0700

    crud book

---
+++ server/models/Book.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+  isbn: {
+    type:String,
+    required: true
+  },
+  title: {
+    type:String,
+    required: true
+  },
+  author: {
+    type:String,
+    required: true
+  },
+  category: {
+    type:String,
+    required: true
+  },
+  stock: {
+    type:Number,
+    required:true
+  },
+  img: {
+    type:String,
+    required:true
+  },
+  price: {
+    type:Number,
+    required:true
+  }
+})
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
