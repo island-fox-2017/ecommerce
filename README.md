c2a2e51be06b0741f2c7162cfbef9830cbd9dcd6
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-08-17 11:34:07 +0700

    update cors

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ecommerce
+
+server side
--- server/app.js
+++ server/app.js
@@ -4,17 +4,22 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const cors = require('cors')
 mongoose.connect('mongodb://localhost/mongoose-crud')
+
 
 const book = require('./routers/book')
 const customer = require('./routers/customer')
 const transaction = require('./routers/transaction')
+
 
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.json({type: 'application/x-www-form-urlencoded'}))
 app.set('view engine', 'ejs');
+
+app.use(cors())
 
 app.get('/', function(req,res){
   res.send('hai ini mongoose')
